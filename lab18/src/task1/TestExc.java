135c149e5bb549f8c8d534fcf9074c341714345b
maisiew <102799104+maisiew@users.noreply.github.com>
2022-11-20 15:01:18 +0300

    Add files via upload

---
+++ lab18/src/task1/TestExc.java
@@ -0,0 +1,8 @@
+package task1;
+
+public class TestExc {
+    public static void main(String[] args) {
+        Exception1 exc = new Exception1();
+        exc.exceptionDemo();
+    }
+}
---
+++ lab18/src/task4/Exc4.java
@@ -0,0 +1,22 @@
+package task4;
+
+import java.util.Scanner;
+
+public class Exc4 {
+    public void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print( "Enter an integer ");
+        String intString = myScanner.next();
+        int i;
+        try{
+            i = Integer.parseInt(intString);
+            System.out.println( 2/i );
+        } catch (NumberFormatException e ) {
+            System.err.println("i is not an int type");
+        } catch ( ArithmeticException e ) {
+            System.err.println("Attempted division by zero");
+        } finally {
+            System.out.println("Вы что-то ввели");
+        }
+    }
+}

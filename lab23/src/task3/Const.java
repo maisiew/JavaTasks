729754ed7c235fb654e320c1cd0c4c8ac6cf6e3b
maisiew <102799104+maisiew@users.noreply.github.com>
2022-11-27 16:28:46 +0300

    Delete lab23 directory

--- lab23/src/task3/Const.java
+++
@@ -1,7 +0,0 @@
-package task3;
-
-public class Const extends Op {
-    public Const(double value){
-        this.value = value;
-    }
-}
--- lab23/src/task3/Op.java
+++
@@ -1,9 +0,0 @@
-package task3;
-
-public abstract class Op {
-    protected double value;
-
-    public double getValue() {
-        return value;
-    }
-}
